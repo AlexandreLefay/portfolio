dc35ab35d0b9f27ee2a9bf21e78aacbe1d5907a8
Alexandre <alexandre.lefay@gmail.com>
2023-10-22 23:39:21 +0200

    init

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import { Link , NavLink } from "react-router-dom"
+import './header.scss'; 
+
+
+export const Header = () => {
+    return (
+        <nav>
+            <a href="#">CREATIVE HUB</a>
+            <a href="#">ABOUT</a>
+      </nav>
+    )
+  }
